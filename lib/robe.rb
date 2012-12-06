4c3c4de758c7fb94f6e4ebc45fb0e01114b54a7f
Dmitry Gutov <dgutov@yandex.ru>
2012-12-06 09:55:17 +0400

    Add Robe.server and Robe.stop; with specs

--- lib/robe.rb
+++ lib/robe.rb
@@ -2,13 +2,22 @@ require "robe/server"
 require "robe/kitchen"
 
 module Robe
-  def self.start(port)
-    return if @server
-    @server = Server.new(port, Kitchen.new)
-    ['INT', 'TERM'].each do |signal|
-      trap(signal) { @server.shutdown; @server = nil }
+  class << self
+    attr_accessor :server
+
+    def start(port)
+      return if @server
+      @server = Server.new(port, Kitchen.new)
+      ['INT', 'TERM'].each do |signal|
+        trap(signal) { stop }
+      end
+      Thread.new { @server.start }
+      nil # no output
     end
-    Thread.new { @server.start }
-    nil # no output
+
+    def stop
+      @server.shutdown
+      @server = nil
+    end
   end
 end
---
+++ spec/robe_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'robe'
+
+describe Robe do
+  before do
+    $stderr = File.new(IO::NULL, "w")
+    Robe.start(12345)
+  end
+
+  after do
+    Robe.stop if Robe.server
+    $stderr = STDERR
+  end
+
+  it "has a server attribute" do
+    expect(Robe.server).to be_a_kind_of(Robe::Server)
+  end
+
+  it "has a stop method" do
+    expect_stop do
+      Robe.stop
+    end
+  end
+
+  %w(INT TERM).each do |signal|
+    it "shuts down on #{signal}" do
+      expect_stop do
+        Process.kill(signal, Process.pid)
+      end
+    end
+  end
+
+  def expect_stop
+    server = Robe.server
+    yield
+    expect(Robe.server).to be_nil
+    expect(server.status).to eq(:Stop)
+  end
+end
