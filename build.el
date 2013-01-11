5939a6411f1dba8fe59396178a59a75297af5d0c
Dmitry Gutov <dgutov@yandex.ru>
2013-01-11 09:17:49 +0400

    Print tar output to stdout

--- build.el
+++ build.el
@@ -42,4 +42,6 @@
         (write-region (point-min) (point-max)
                       (format "%s/%s-pkg.el" dir name)))
       (call-process "tar" nil "*robe-build*" nil "-cvf"
-                    (format "%s.tar" dir) dir))))
+                    (format "%s.tar" dir) dir)
+      (with-current-buffer "*robe-build*"
+        (princ (buffer-string))))))
