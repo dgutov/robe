c5a0ae6f68c699ec3cdbb73a3e01e76f62d75f48
Dmitry Gutov <dgutov@yandex.ru>
2015-11-20 00:58:38 +0200

    Bump the version

--- robe.el
+++ robe.el
@@ -5,7 +5,7 @@
 
 ;; Author: Dmitry Gutov
 ;; URL: https://github.com/dgutov/robe
-;; Version: 0.7.7
+;; Version: 0.7.8
 ;; Keywords: ruby convenience rails
 ;; Package-Requires: ((inf-ruby "2.3.0"))
 
