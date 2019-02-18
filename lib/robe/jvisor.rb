74a78eb4bc7f6384a7e946d6d0478256cd696612
Dmitry Gutov <dgutov@yandex.ru>
2019-02-18 17:22:16 +0300

    JVisor#each_object: Check for instance_methods
    It's a better indicator

--- lib/robe/jvisor.rb
+++ lib/robe/jvisor.rb
@@ -3,8 +3,8 @@ require 'robe/visor'
 module Robe
   class JVisor < Visor
     def each_object(mod)
-      # http://jira.codehaus.org/browse/JRUBY-7027
-      ObjectSpace.each_object(mod).select { |m| m.respond_to? :name }
+      # JRuby has weird Module's (Java package objects, etc)
+      ObjectSpace.each_object(mod).select { |m| m.respond_to? :instance_methods }
     end
 
     def descendants(cls)
