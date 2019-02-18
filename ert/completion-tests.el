26e359168c85fc2eeb30c4194cbf6047705ae9ce
Dmitry Gutov <dgutov@yandex.ru>
2019-02-18 17:05:09 +0300

    Update completion-tests

--- ert/completion-tests.el
+++ ert/completion-tests.el
@@ -12,7 +12,8 @@ class A
 end")
     (ruby-mode)
     (should (equal '("@f2" "@f1")
-                   (robe-complete--instance-variables)))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--instance-variables))))))
 
 (ert-deftest complete-instance-variables-ignores-comments ()
   (with-temp-buffer
@@ -25,7 +26,8 @@ class A
 end")
     (ruby-mode)
     (should (equal '("@f2")
-                   (robe-complete--instance-variables)))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--instance-variables))))))
 
 (ert-deftest complete-local-variables-collects-assignments-in-method ()
   (with-temp-buffer
@@ -40,7 +42,8 @@ class A
     xyz = 5 # def bar fake
     ")
     (should (equal '("xyz" "tee")
-                   (robe-complete--local-variables "bar")))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--local-variables "bar"))))))
 
 (ert-deftest complete-local-variables-in-class-method ()
   (with-temp-buffer
@@ -50,7 +53,8 @@ class A
     abc = 1
     ")
     (should (equal '("abc" "ccc")
-                   (robe-complete--local-variables "foo")))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--local-variables "foo"))))))
 
 (ert-deftest complete-local-variables-works-outside-of-methods ()
   (with-temp-buffer
@@ -59,7 +63,8 @@ abc = 1
 tee = 4 + qux
 ")
     (should (equal '("tee" "abc")
-                   (robe-complete--local-variables nil)))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--local-variables nil))))))
 
 (ert-deftest complete-local-variables-includes-method-arguments ()
   (insert "
@@ -68,7 +73,7 @@ class A
     xyz = 5
     ")
     (should (equal '("xyz" "rest" "a3" "a2" "a1")
-                   (robe-complete--local-variables "foo"))))
+                   (mapcar #'robe--variable-name (robe-complete--local-variables "foo")))))
 
 (ert-deftest complete-local-variables-includes-block-arguments ()
   (insert "
@@ -77,4 +82,5 @@ class A
     [a1, a2].blarg do |b, z, (x, c)|
     ")
     (should (equal '("c" "x" "z" "b")
-                   (robe-complete--local-variables "foo"))))
+                   (mapcar #'robe--variable-name
+                           (robe-complete--local-variables "foo")))))
