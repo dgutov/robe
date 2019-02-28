a3f1a8008f80cbad43cd6d323eaee06b38c4b65b
Dmitry Gutov <dgutov@yandex.ru>
2019-02-28 12:32:01 +0300

    Reformat a little

--- lib/robe/sash/const_locations.rb
+++ lib/robe/sash/const_locations.rb
@@ -11,20 +11,24 @@ module Robe
 
       def all(name, mod)
         locations = {}
-        if (obj = visor.resolve_context(name, mod)) and obj.is_a? Module
+
+        if (obj = visor.resolve_context(name, mod)) and obj.is_a?(Module)
           methods = obj.methods(false).map { |m| obj.method(m) } +
                     obj.__instance_methods__(false).map { |m| obj.instance_method(m) }
+
           methods.each do |m|
-            if loc = m.source_location
+            if (loc = m.source_location)
               path = loc[0]
               locations[path] ||= 0
               locations[path] += 1
             end
           end
         end
-        if defined? Class.class_attribute and Class != obj
-          locations.delete Class.method(:class_attribute).source_location[0]
+
+        if defined?(Class.class_attribute) && Class != obj
+          locations.delete(Class.method(:class_attribute).source_location[0])
         end
+
         locations.keys.sort { |k1, k2| -(locations[k1] <=> locations[k2]) }
       end
     end
