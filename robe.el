ab3391bbec75a8ab5ab40dfedb231a47784057b4
Dmitry Gutov <dmitry@gutov.dev>
2023-10-21 03:44:40 +0300

    Remove unnecessary call (requests are silenced by the arg)

--- robe.el
+++ robe.el
@@ -264,7 +264,6 @@ project."
                                          (t "-")))
                                  args "/")))
          (response-buffer (robe-retrieve url)))
-    (message nil) ;; So "Contacting host" message is cleared
     (if response-buffer
         (prog1
             (with-temp-buffer
